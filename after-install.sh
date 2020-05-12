8c0c406553083b82547b4f96a44f3c1513c16fd4
SinaPoyanfar <sinapoyanfar1379@gmail.com>
2020-05-13 01:12:36 +0430

    added configs

--- after-install.sh
+++ after-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+read -p 'Enter your Ubuntu username: ' user
+export CURRENT_USER=$user
+
 # check for user as root
 if ! [ $(id -u) = 0 ]; then
    echo "The script need to be run as root." >&2
@@ -13,8 +16,8 @@ else
 fi
 
 # read user git details
-read -sp 'git username: ' git_username
-read -sp 'git useremail: ' git_useremail
+read -p 'git username: ' git_username
+read -p 'git useremail: ' git_useremail
 
 # updating cores
 apt update
@@ -29,26 +32,26 @@ apt install apt-transport-https curl
 apt install git -y
 git config --global user.name "${git_username}"
 git config --global user.email "${git_useremail}"
-echo "\033[43m \033[30m you need to add gpg and ssh key manually. \033[0m"
+echo -e "\033[43m \033[30m you need to add gpg and ssh key manually. \033[0m"
 
 # programming stuff
 apt install gcc g++ -y
 apt install vim -y
-sudo apt-get install python-software-properties -y
 apt install python3-pip -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # proxy stuff
 apt install privoxy -y
-# TODO: add config later
-apt install shadowsocks -y
-# todo: check for errors
+mv /etc/privoxy/config /etc/privoxy/config.bak
+cp -v ./configs/privoxy/config /etc/privoxy/config
+# TODO: install shadowsocks 
 
 # installing vscode
-# TODO: search about extensions installation with commandline
-# ToDO: search about were global config is saved 
 snap install code --classic
+sh ./configs/vscode/extensions.sh 
+mv /home/$user/.config/Code/User/settings.json /home/$user/.config/Code/User/settings.json.bak
+cp -v ./configs/vscode/settings.json /home/$user/.config/Code/User/settings.json
 
 # installing browsers
 snap install brave
@@ -56,16 +59,19 @@ snap install chromium
 
 apt install unzip
 
-# installig zsh and oh-my-zsh with hack font
+# installig zsh with hack font
 apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# TODO: config zsh later
 chsh -s $(which zsh)
 curl -o /tmp/fonts/x-hack.tar.gz https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.tar.gz
 tar -xzf /tmp/fonts/x-hack.tar.gz 
-mkdir ~/.fonts
-mv /tmp/fonts/hack* ~/.fonts/ 
+mkdir /home/$user/.fonts
+mv /tmp/fonts/hack* /home/$user/.fonts/ 
 fc-cache -f -v
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$user/zsh-syntax-highlighting
+mv /home/$user/.zshrc /home/$user/.zshrc.bak
+cp ./configs/zsh/zshrc /home/$user/.zshrc 
 
 # installing player
 snap install --beta mpv 
@@ -81,3 +87,13 @@ snap install discord
 
 # installing android studio
 snap install android-studio --classic
+
+apt autoclean
+apt autoclear
+
+echo -e "\033[43m \033[30m for affective and make zsh your permanent shell reboot your system. \033[0m"
+
+# lastly oh-my-zsh 
+sudo -u $user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo -e "\033[43m \033[30m for affective and make zsh your permanent shell reboot your system. \033[0m"
