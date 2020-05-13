e3069580c8a9c86eec2b2003b7f01ddaab54af8b
ashkan4472 <ashkan4472@gmail.com>
2020-05-13 21:31:17 +0430

    added user detail

--- after-install.sh
+++ after-install.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-read -p 'Enter your Ubuntu username: ' user
-export CURRENT_USER=$user
-
-# check for user as root
-if ! [ $(id -u) = 0 ]; then
-   echo "The script need to be run as root." >&2
-   exit 1
-fi
-
-if [ $SUDO_USER ]; then
-    real_user=$SUDO_USER
-else
-    real_user=$(whoami)
-fi
+CURRENT_USER=$1
+export CURRENT_USER=$1
 
 # read user git details
 read -p 'git username: ' git_username
@@ -57,8 +44,8 @@ cp -v ./configs/apt/proxy.conf /etc/apt/apt.conf.d/proxy.conf
 # installing vscode
 snap install code --classic
 sh ./configs/vscode/extensions.sh 
-mv /home/$user/.config/Code/User/settings.json /home/$user/.config/Code/User/settings.json.bak
-cp -v ./configs/vscode/settings.json /home/$user/.config/Code/User/settings.json
+mv /home/$CURRENT_USER/.config/Code/User/settings.json /home/$CURRENT_USER/.config/Code/User/settings.json.bak
+cp -v ./configs/vscode/settings.json /home/$CURRENT_USER/.config/Code/User/settings.json
 
 # installing browsers
 snap install brave
@@ -71,14 +58,14 @@ apt install zsh -y
 chsh -s $(which zsh)
 curl -o /tmp/fonts/x-hack.tar.gz https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.tar.gz
 tar -xzf /tmp/fonts/x-hack.tar.gz 
-mkdir /home/$user/.fonts
-mv /tmp/fonts/hack* /home/$user/.fonts/ 
+mkdir /home/$CURRENT_USER/.fonts
+mv /tmp/fonts/hack* /home/$CURRENT_USER/.fonts/ 
 fc-cache -f -v
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$user/zsh-syntax-highlighting
-mv /home/$user/.zshrc /home/$user/.zshrc.bak
-cp ./configs/zsh/zshrc /home/$user/.zshrc 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$CURRENT_USER/zsh-syntax-highlighting
+mv /home/$CURRENT_USER/.zshrc /home/$CURRENT_USER/.zshrc.bak
+cp ./configs/zsh/zshrc /home/$CURRENT_USER/.zshrc 
 
 # installing player
 snap install --beta mpv 
@@ -105,11 +92,11 @@ done
 # installing docker
 apt remove docker docker-engine docker.io containerd runc -y
 apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-sudo -u $user curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo -u $CURRENT_USER curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install docker-ce docker-ce-cli containerd.io
-usermod -aG docker $user
+usermod -aG docker $CURRENT_USER
 
 # installing variety
 add-apt-repository ppa:peterlevi/ppa -y
@@ -123,6 +110,6 @@ apt autoclear
 echo -e "\033[43m \033[30m for affective and make zsh your permanent shell reboot your system. \033[0m"
 
 # lastly oh-my-zsh 
-sudo -u $user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo -u $CURRENT_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo -e "\033[43m \033[30m for affective and make zsh your permanent shell reboot your system. \033[0m"
---
+++ runme.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo chmod +x after-install.sh
+sudo ./after-install.sh $USER
